a8ad79d2d23ff22b4373dce389583001363dedd5
Sondro <memoiran@hotmail.com>
2018-03-04 16:38:19 -0800

    Add files via upload

---
+++ SkeletonD.hx
@@ -0,0 +1,39 @@
+package;
+import kha.math.FastMatrix4;
+
+/**
+ * ...
+ * @author Joaquin
+ */
+class SkeletonD
+{
+	public var transformation:FastMatrix4;
+	public var bones:Array<Bone>;
+	public var ID:String;
+	public var result:Bone;
+
+	public inline function new() 
+	{
+		transformation = FastMatrix4.identity();
+		bones = new Array();	
+	}
+	public inline function setFrame(aFrame:Int)
+	{
+		for (child in bones) 
+		{
+			child.setFrame(aFrame, transformation);
+		}
+	}
+	public function getBone(aId:String):Bone
+	{	
+		for (child in bones) 
+		{
+			if (child.id == aId) { return child; }
+			result = child.getBone(aId);
+			if (result != null) { return result; }
+		}
+		return null;
+	}
+	
+	
+}
