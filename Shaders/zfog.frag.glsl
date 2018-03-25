7f672cf5741d70302236770e7551250047a40de3
Sondro <memoiran@hotmail.com>
2018-03-25 10:39:33 -0700

    commit

--- Shaders/zfog.frag.glsl
+++ Shaders/zfog.frag.glsl
@@ -1,8 +1,9 @@
 #version 450
 uniform sampler2D tex;
 
-float uFogDistance;
-float vFogClampedDistance;   
+in float uFogDistance;
+in float vFogClampedDistance;   
+
 vec4 textureColor;
 vec4 color;
 
@@ -12,12 +13,12 @@ in vec2 texCoord;
 
 void kore() {
 
-   textureColor = texture(tex, vec2(texCoord.s, texCoord.t));       
+   textureColor = texture(tex, texCoord);       
 
    if (uFogDistance == 0.0) {                                                                   
       //color = ...use only lighting, not fog
    }                               
    else {                          
-      //color = mix(final_color, vec4(0.5,0.5,0.5,1.0), vFogClampedDistance/uFogDistance) * vec4(1.0,1.0,1.0,textureColor.a);  
+      color = mix(textureColor, vec4(0.5,0.5,0.5,1.0), vFogClampedDistance/uFogDistance) * vec4(1.0,1.0,1.0,textureColor.a);  
    }  
 }
