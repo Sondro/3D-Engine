38b69b7cd3de035c5482220e30b8fb08da6cbe3c
Sondro <memoiran@hotmail.com>
2018-03-25 11:34:28 -0700

    commit

--- Shaders/zfog.vert.glsl
+++ Shaders/zfog.vert.glsl
@@ -1,11 +1,12 @@
 #version 450
 
-out float uFogDistance;  
+uniform float uFogDistance = 0.5;  
 
 out float vFogClampedDistance;   
 out vec2 texCoord;
 
 void kore() {
+   //uFogDistance = 0.5; 
    if(uFogDistance >= 0.0) {                                        
       vFogClampedDistance = clamp(gl_Position.z, 0.0, uFogDistance); 
    }                                                              
